640b72c108e448c5b5b88164890f9593a74f8d80
Гафеев Глеб <gleb.gafeev@mail.ru>
2022-04-28 15:27:18 +0300

    HashTable

---
+++ Lesson1/HashTable/HashTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1.HashTable
+{
+    public class HashTable<TValue> : IAssocArray<int, TValue>
+    {
+        private List<TValue>[] array;
+
+        public HashTable(int maxKeyValue)
+        {
+            array = new List<TValue>[maxKeyValue];
+        }
+
+        public void Insert(int key, TValue value)
+        {
+            if (array[key] == null)
+                array[key] = new List<TValue>();
+            array[key].Add(value);
+        }
+
+        public bool Find(int key, TValue value)
+        {
+            return array[key].Contains(value);
+        }
+
+        public void Remove(int key, TValue value)
+        {
+            array[key].Remove(value);
+        }
+        public void Clear()
+        {
+            foreach (var list in array)
+                list.Clear();
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var filledLists = array.Where(x => x != null);
+            foreach (var l in filledLists)
+                foreach (var el in l)
+                    sb.Append(el.ToString() + " ");
+            return sb.ToString();
+        }
+    }
+}
